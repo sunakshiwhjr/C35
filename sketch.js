2930cb69168141d64da4f9f63d6d40ba72cb60ad
sunakshiwhjr <77574525+sunakshiwhjr@users.noreply.github.com>
2021-10-22 09:34:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var hypnoticBall, database;
+
+function setup(){
+
+    database = firebase.database();
+    console.log(database);
+
+    createCanvas(500,500);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+
+    var hypnoticBallPosition = database.ref('ball/position');
+    hypnoticBallPosition.on("value", readPosition, showError)
+}
+
+function draw(){
+    background("black");
+    if(position !== undefined)
+    {
+
+    
+    text(mouseX + "," + mouseY, mouseX, mouseY);
+    console.log("x Position", hypnoticBall.x);
+    console.log("y position", hypnoticBall.y)
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+}
+
+function readPosition(data)
+{
+    position = data.val();
+    console.log(position.x);
+
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+}
+
+
+function writePosition(x,y)
+{
+    database.ref('ball/position').set({
+
+        'x': position.x + x,
+        'y': position.y + y
+    })
+}
+
+function showError(){
+    console.log("Error in writing to the database");
+  }
